1ed488589c35232631abe743b95ae6523013a5b5
ValeryShvyndzikau <valery_shvyndzikau@epam.com>
2021-01-03 21:27:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# handmade-redux
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/handmade-redux)
---
+++ index.js
@@ -0,0 +1,85 @@
+const createStore = (reducer, preloadedState = {}) => {
+  let state = preloadedState;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(l => l(state));
+  };
+
+  // const subcribe = (listener) => listenetrs = [...listeners, listener];
+  // const unsubscribe = (listener) => listeners.filter(l !== listener);
+  // let see alternative unsubscribe method here
+
+  const subscribe = listener => {
+    listenetrs = [...listeners, listener];
+    return () => listeners.filter(l !== listener);
+  };
+
+  dispatch({ type: "INIT" }); // itial state population, we get to reduce to make initial state
+
+  return {
+    getState,
+    dispatch,
+    subscribe
+    //unsubscribe
+  };
+};
+
+combineReducers({
+  somePage: (state, action) => action
+});
+
+// you call it inside the each module, when difine it
+const combineReducers = reducers => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      nextState,
+      key => {
+        nextState[key] = reducers[key](state[key], action);
+
+        return nextState;
+      },
+      {}
+    );
+  };
+};
+
+/*
+
+//
+// Wrap every object before passing to createReducer
+
+export default function createReducer(initialState: ?{}, handlers: {}) {
+  return function reducer(state: ?{} = initialState, action: {type: string, payload: Object}) {
+    return typeof handlers[action.type] === 'function'
+        ? handlers[action.type](state, action)
+        : state;
+  };
+}
+
+*/
+
+// initial state populating model
+const someReducer = (state = "default value", action) => {
+  if ((action.type = "bla-bla-bla")) {
+    // do updates
+    // return new state
+  }
+
+  return state;
+};
+
+/**
+ * @todo: Clarify realisation details
+ */
+const connect = (mapStateToProps, mapDispatchToProps) => Component => {
+  return ({ context, ...ownProps }) => {
+    const { state, dispatch } = component.context;
+    const props = mapStateToProps(state, ownProps);
+
+    return <Component {...props} {...ownProps} dispatch={dispatch} />;
+  };
+};
